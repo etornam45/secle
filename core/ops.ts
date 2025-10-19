ede26d27ab727218f45323f082758dfa8390c7cb
Etornam <navidben45@gmail.com>
2025-10-19 19:50:27 +0000

    Added Support for scalar-element wise multiplication

--- core/ops.ts
+++ core/ops.ts
@@ -56,27 +56,51 @@ export function neg(a: Tensor): Tensor {
 }
 
 
-export function mul(a: Tensor, b: Tensor): Tensor {
-  const out = CPU.mul(a, b);
-  out._prev = [a, b];
-  out.requires_grad = a.requires_grad || b.requires_grad;
-  
-  out._backward = () => {
-    if (!out._grad) return;
-    if (a.requires_grad) {
-      const a_grad = mul(a, out._grad);
-      a._grad = a._grad ? add(a._grad, a_grad) : a_grad;
-    }
-    if (b.requires_grad) {
-      const b_grad = mul(out._grad, a);
-      b._grad = b._grad ? add(b._grad, b_grad) : b_grad;
-    }
-  };
-  
-  return out;
+export function mul(a: Tensor | number, b: Tensor | number): Tensor {
+
+  if (typeof a === "number" && b instanceof Tensor) {
+    return CPU.mul_scalar(b, a);
+  }
+  if (typeof b === "number" && a instanceof Tensor) {
+    return CPU.mul_scalar(a, b);
+  }
+
+  if (a instanceof Tensor && b instanceof Tensor) {
+    const out = CPU.mul(a, b);
+    out._prev = [a, b];
+    out.requires_grad = a.requires_grad || b.requires_grad;
+
+    out._backward = () => {
+      if (!out._grad) return;
+      if (a.requires_grad) {
+        const a_grad = mul(a, out._grad);
+        a._grad = a._grad ? add(a._grad, a_grad) : a_grad;
+      }
+      if (b.requires_grad) {
+        const b_grad = mul(out._grad, a);
+        b._grad = b._grad ? add(b._grad, b_grad) : b_grad;
+      }
+    };
+
+    return out;
+  }
+  // if both are numbers, return a new tensor with the product
+  if (typeof a === "number" && typeof b === "number") {
+    return new Tensor([a * b], [1, 1]);
+  }
+
+  throw new Error("Invalid arguments to mul. Expected a Tensor or a number, got " + typeof a + " and " + typeof b);
 }
 
-export function add(a: Tensor, b: Tensor): Tensor {
+  }
+  if (typeof a === "number" && b instanceof Tensor) {
+    return CPU.add_a_number(b, a);
+  }
+  if (typeof b === "number" && a instanceof Tensor) {
+    return CPU.add_a_number(a, b);
+  }
+
+  if (a instanceof Tensor && b instanceof Tensor) {
   const out = CPU.add(a, b);
   out._prev = [a, b];
   out.requires_grad = a.requires_grad || b.requires_grad;
